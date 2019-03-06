bc0680220401bfbdcd70d5a5f446fe71a7f2e40c
lenlag <lenlag@yandex.ru>
2019-03-06 17:20:09 +0100

    init

---
+++ NatApp/app/src/main/java/formation/afpa/natapp/MainActivity.java
@@ -0,0 +1,114 @@
+package formation.afpa.natapp;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.Arrays;
+import java.util.List;
+
+import model.DataManager;
+import model.Movie;
+
+public class MainActivity extends AppCompatActivity {
+
+    DataManager data = new DataManager();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ListView list = findViewById(R.id.list);
+
+        List films = Arrays.asList(data.getMovie());
+
+        ArrayAdapter adapter = new ArrayAdapter<Movie>(this, android.R.layout.simple_list_item_1, films);
+
+        list.setAdapter(adapter);
+
+
+//        list.setOnClickListener(new AdapterView.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//
+//            @Override
+//            public void onItemClick(View v) {
+//                Intent intent = new Intent().setClass(MainActivity.this, DetailsActivity.class);
+//                intent.putExtra("MY_FILMS", data.getMovie());
+//                startActivity(intent);
+//            }
+//        });
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Movie movie = (Movie)parent.getAdapter().getItem(position);
+                Intent intent = new Intent().setClass(MainActivity.this, DetailActivity.class);
+                intent.putExtra("MY_FILM", movie);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.dialogTitle).setMessage(R.string.quit);
+
+        builder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                MainActivity.super.onBackPressed(); // ne pas utiliser onDestroy()=> l'appli ne cesse de s'arreter
+                dialog.dismiss();
+            }
+        });
+
+        builder.setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        AlertDialog dlg = builder.create();
+        dlg.show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
