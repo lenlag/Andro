11a3d03aa97a22dacdc65ca860b0becaad00c74a
lenlag <lenlag@yandex.ru>
2019-03-13 11:44:08 +0100

    NatSpecies

---
+++ NatSpecies/app/src/main/java/formation/afpa/natspecies/MainActivity.java
@@ -0,0 +1,71 @@
+package formation.afpa.natspecies;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Specie;
+
+public class MainActivity extends AppCompatActivity {
+
+    private List <Specie> specielist = new ArrayList<Specie>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+        TextView welcomeText = findViewById(R.id.textViewWelcome);
+        TextView textSpecies = findViewById(R.id.textView2);
+
+        ListView listSpecies = findViewById(R.id.listView);
+        listSpecies.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                
+                Intent intent = new Intent().setClass(MainActivity.this, CreateOrUpdateActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ NatSpecies/app/src/main/java/model/ListSpeciesThread.java
@@ -0,0 +1,9 @@
+package model;
+
+public class ListSpeciesThread extends Thread {
+
+    @Override
+    public void run() {
+        super.run();
+    }
+}
